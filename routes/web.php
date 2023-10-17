2e55af2afe22c86c9fb618f4ad005ba233000648
Joe <104938042+lrljoe@users.noreply.github.com>
2023-10-17 22:21:48 +0100

    Removing unrequired files

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', \App\Http\Livewire\Demos\NewPage::class)->name('pg');
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('welcome');
 Route::get('/tw2', \App\Http\Livewire\Demos\Tailwind2::class)->name('tw2');
 Route::get('/tw3', \App\Http\Livewire\Demos\Tailwind3::class)->name('tw3');
 Route::get('/bs4', \App\Http\Livewire\Demos\Bootstrap4::class)->name('bs4');
 Route::get('/bs5', \App\Http\Livewire\Demos\Bootstrap5::class)->name('bs5');
-Route::get('/pg', \App\Http\Livewire\Demos\NewPage::class)->name('pg');
+
+Route::get('/welcome', function () {
+    return view('welcome');
+})->name('welcome');
