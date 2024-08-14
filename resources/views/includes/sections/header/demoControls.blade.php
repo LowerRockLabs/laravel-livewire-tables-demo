fd29f0793cc39e90edeffeb14af709c87fe91ae6
lrljoe <joe@lowerrocklabs.com>
2024-08-14 05:51:38 +0100

    Tweaks to Files

--- resources/views/includes/sections/header/demoControls.blade.php
+++ resources/views/includes/sections/header/demoControls.blade.php
@@ -3,7 +3,6 @@
         <h1 style="font-size: 2em">Rappasoft Livewire Table Demo</h1>
         <h2 style="font-size: 1.3em;"><strong>App Name</strong>: {{ config('app.name') }}</h2>
         <h2 style="font-size: 1.3em;"><strong>Package Version</strong>: {{ ltrim(ltrim(\Illuminate\Support\Facades\Process::run("cd .. && composer show rappasoft/laravel-livewire-tables | grep 'versions : '")->output(), 'versions :'), '* ') ?? 'Unknown' }} </h2>
-        
         <div style='margin-top: 2em'>
             <span style="font-size: 1.25em; font-weight: bold;">Change Theme</span>
             <span style="padding: 1em"><a href='tw2'>Tailwind 2</a></span>
@@ -12,7 +11,10 @@
             <span style="padding: 1em"><a href='bs5'>Bootstrap 5</a></span>
         </div>
             
-        <div class="inline-flex flex-cols px-4 mx-4"  style='margin-top: 2em'>
+        <div @class([
+                'inline-flex flex-cols px-4 mx-4' => ($this->theme == 'tailwind' || $this->theme == 'tw3' || $this->theme == 'tw2'),
+                'd-inline-flex  flex-row space-x-2 align-items-center align-content-center mx-2 px-2 py-2' =>  ($this->theme == 'bs4' || $this->theme == 'bs5' || $this->theme == 'bootstrap' || $this->theme == 'bootstrap-4' || $this->theme == 'bootstrap-5'),
+            ])  style='margin-top: 2em'>
             <div style="margin: 1em">
                 @include('includes.sections.header.controls.tableSwitcher')
             </div>
--- resources/views/includes/sections/header/info/currentversion.blade.php
+++
@@ -1,5 +0,0 @@
-@php
-$packageVersion = ltrim(ltrim(\Illuminate\Support\Facades\Process::run("cd .. && php composer.phar show rappasoft/laravel-livewire-tables | grep 'versions : '")->output(), 'versions :'), '* ');
-@endphp
-
-<h5>Core Package Branch/Version: {{ $packageVersion }}</h5>
