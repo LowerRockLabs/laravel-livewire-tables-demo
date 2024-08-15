15826e7718b43910ca4558eea864e741ca68d1a7
lrljoe <joe@lowerrocklabs.com>
2024-08-15 20:35:15 +0100

    Update Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,16 @@
 <div>
     <div>
-        <span style="font-size: 1.5em; font-weight: bold;">Change Theme</span>
+        <p>Welcome to the Table Demo for Rappasoft Laravel Livewire Tables v3</p>
+
+        <p>Choose a Theme below, to view examples of the tables</p>
+        
+    </div>
+    <div>
+        <span style="font-size: 1.5em; font-weight: bold;">Choose Theme</span>
         <a href='tw2'>Tailwind 2</a>
         <a href='tw3'>Tailwind 3</a>
         <a href='bs4'>Bootstrap 4</a>
         <a href='bs5'>Bootstrap 5</a>
-    </div>
-    <div class="flex flex-cols px-4 mx-4">
-        <div style="margin: 1em">@include('includes.sections.header.controls.tableSwitcher')</div>
     </div>
 </div>
 
