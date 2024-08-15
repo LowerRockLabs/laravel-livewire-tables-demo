8739b32f8e4eed87791bf8e68cdfb969f6db7fe2
lrljoe <joe@lowerrocklabs.com>
2024-08-15 20:41:07 +0100

    Remove Top Margin

--- resources/views/page/tablecomponent.blade.php
+++ resources/views/page/tablecomponent.blade.php
@@ -1,4 +1,4 @@
-<div style="margin-top: 5em;">
+<div style="margin-top: 2em;">
     @include('includes.sections.header.demoControls')
     <div class="h-screen" wire:key="{{ $filterDemoKey }}-wrapper">
         <livewire:dynamic-component :key="$filterDemoKey" :is="$selectedTable" theme="{{ $tableTheme }}"  />
