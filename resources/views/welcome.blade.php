5c14bfc92949fb66a0d49656e0ce6a7d56244a16
lrljoe <joe@lowerrocklabs.com>
2024-08-15 20:36:00 +0100

    Update Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,10 +6,10 @@
         
     </div>
     <div>
-        <span style="font-size: 1.5em; font-weight: bold;">Choose Theme</span>
-        <a href='tw2'>Tailwind 2</a>
-        <a href='tw3'>Tailwind 3</a>
-        <a href='bs4'>Bootstrap 4</a>
+        <p style="font-size: 1.5em; font-weight: bold;">Choose Theme</p>
+        <a href='tw2'>Tailwind 2</a><br />
+        <a href='tw3'>Tailwind 3</a><br />
+        <a href='bs4'>Bootstrap 4</a><br />
         <a href='bs5'>Bootstrap 5</a>
     </div>
 </div>
