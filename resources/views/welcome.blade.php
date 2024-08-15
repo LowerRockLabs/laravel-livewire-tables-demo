414d27d010da73418b607488a160f06f76c4c9b0
lrljoe <joe@lowerrocklabs.com>
2024-08-15 20:37:23 +0100

    Update Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 <div>
+    <p style="font-size: 1.5em; font-weight: bold;">Laravel Livewire Tables Demo</p>
+
     <div>
         <p>Welcome to the Table Demo for Rappasoft Laravel Livewire Tables v3</p>
-
         <p>Choose a Theme below, to view examples of the tables</p>
-        
     </div>
     <div>
-        <p style="font-size: 1.5em; font-weight: bold;">Choose Theme</p>
+        <p style="font-size: 1.25em; font-weight: bold;">Choose A Theme</p>
         <a href='tw2'>Tailwind 2</a><br />
         <a href='tw3'>Tailwind 3</a><br />
         <a href='bs4'>Bootstrap 4</a><br />
